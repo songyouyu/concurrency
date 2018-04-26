177c852c86dd25bdcfe439b1539d0cde5c0e71ff
songyouyu <songyy950208@163.com>
2018-04-26 10:17:03 +0800

    项目准备

---
+++ src/main/java/com/mmall/concurrency/TestController.java
@@ -0,0 +1,20 @@
+package com.mmall.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * created by song on 2018/4/25
+ */
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
---
+++ src/main/java/com/mmall/concurrency/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * created by song on 2018/4/26
+ */
+package com.mmall.concurrency.example;
